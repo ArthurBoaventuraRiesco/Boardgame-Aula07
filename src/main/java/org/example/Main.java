de67daebb2971bacff993512aa9b7bcc8ad9f3d5
Arthur Boaventura Riesco <riesco.arthur2005@gmail.com>
2025-09-29 23:52:32 -0300

    Corrigindo erros (refs#7)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         BoardGameCatalog catalog = new BoardGameCatalog();
 
-        // cria alguns jogos
+
         BoardGame azul = BoardGame.of(
                 "Azul", 2, 4, 45, "Plan B", 2017, Set.of("pattern building"), 8.0
         );
@@ -15,14 +15,14 @@ public class Main {
                 "Catan", 3, 4, 75, "Kosmos", 1995, Set.of("trading", "tile placement"), 8.2
         );
 
-        // adiciona no catálogo
+
         catalog.add(azul);
         catalog.add(catan);
 
-        // imprime top 1
+
         System.out.println("Top rated: " + catalog.topRated(1));
 
-        // imprime tempo médio
+
         System.out.println("Tempo médio de jogo: " + catalog.averagePlayTime() + " minutos");
     }
 }
